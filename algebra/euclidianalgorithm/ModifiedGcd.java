4d1ac987933eac74f7c8e679fdb56aa8132f253b
B Soma Naik <somanaik2411@gmail.com>
2021-06-10 20:10:02 +0800

    add main logic to solve modified gcd

--- algebra/euclidianalgorithm/ModifiedGcd.java
+++ algebra/euclidianalgorithm/ModifiedGcd.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class ModifiedGcd {
@@ -36,4 +39,53 @@ public class ModifiedGcd {
     static InputReader in = new InputReader(System.in);
     static PrintWriter out = new PrintWriter(System.out);
 
+    static int gcd(int a, int b) {
+        while(b > 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+
+    static void solve() {
+        int a = in.nextInt(), b = in.nextInt();
+        List<Integer> arr = new ArrayList<>();
+        int g = gcd(a, b);
+        for(int i = 1; i*i <= g; i++) {
+            if(g%i == 0) {
+                int j = g / i;
+                arr.add(i);
+                if(j != i) arr.add(j);
+            }
+        }
+        Collections.sort(arr);
+        int n = in.nextInt();
+        while(n > 0) {
+            int low = in.nextInt(), high = in.nextInt(), res = -1;
+            int l = 0, h = arr.size() - 1;
+            while(l < h) {
+                int m = (l + h) >> 1;
+                if(arr.get(m) <= high) {
+                    l = m + 1;
+                } else h = m;
+            }
+            if(arr.get(l) > high) l--;
+            if(l >= 0) {
+                if(arr.get(l) >= low) {
+                    res = arr.get(l);
+                }
+            }
+            out.println(res);
+            n--;
+        }
+    }
+    public static void main(String[] args) {
+        int tests = 1;
+//        tests = in.nextInt();
+        for(int i = 1; i <= tests; i++) {
+            solve();
+        }
+        out.close();
+    }
 }
